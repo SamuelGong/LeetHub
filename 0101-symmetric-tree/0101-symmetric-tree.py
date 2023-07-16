2689795e38663dd7e547a2e4e52d452d107ff8ff
Zhifeng Jiang <samuelgong2017@gmail.com>
2023-07-16 21:56:39 +0800

    Time: 51 ms (55.33%), Space: 16.4 MB (81.71%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -5,49 +5,37 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSymmetric(self, root: Optional[TreeNode]) -> bool:        
-        levelorder_result = []
-        sentinel = -101
+    
+    
+    def postorder(self, root, mode="left"):
+        magic = -101
+        if root is None:
+            return [magic]
         
-        queue = [root]
-        level_queue = [0]
-        while len(queue) > 0:
-            node = queue.pop(0)
-            level = level_queue.pop(0)
-            
-            if len(levelorder_result) < level + 1:
-                if node is not None:
-                    levelorder_result.append([node.val])
-                else:
-                    levelorder_result.append([sentinel])
-            else:
-                if node is not None:
-                    levelorder_result[-1].append(node.val)
-                else:
-                    levelorder_result[-1].append(sentinel)
-
-            if node is not None:
-                if node.left is not None:
-                    queue.append(node.left)
-                else:
-                    queue.append(None)
-                level_queue.append(level + 1)
-
-                if node.right is not None:
-                    queue.append(node.right)
-                else:
-                    queue.append(None)
-                level_queue.append(level + 1)
-                    
-        if len(levelorder_result) == 2:
-            return True  # only root node
-            
-        # at least two levels
-        for level in levelorder_result[1:]:
-            b, e = 0, len(level ) - 1
-            while b < e:
-                if not level[b] == level[e]:
-                    return False
-                b += 1
-                e -= 1
+        result = []
+        if mode == "left":
+            result += self.postorder(root.left, mode)
+            result += self.postorder(root.right, mode)
+        else:
+            result += self.postorder(root.right, mode)
+            result += self.postorder(root.left, mode)        
+        result += [root.val]
+        return result
+    
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if root.left is None and root.right is None:
+            return True
+        if not (root.left is not None and root.right is not None):
+            return False
+        
+        left_subtree = self.postorder(root.left, "left")
+        right_subtree = self.postorder(root.right, "right")
+        
+        if not len(left_subtree) == len(right_subtree):
+            return False
+        for i, l_e in enumerate(left_subtree):
+            r_e = right_subtree[i]
+            if not l_e == r_e:
+                return False
         return True
+        
