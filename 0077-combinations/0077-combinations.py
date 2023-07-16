ef3f10816e0c708990836f13505dae7ca99fe27a
Zhifeng Jiang <samuelgong2017@gmail.com>
2023-07-17 00:12:22 +0800

    Time: 105 ms (84.62%), Space: 18.1 MB (96.77%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -4,10 +4,13 @@ class Solution:
         self.path = []
         
     def backtracking(self, b, e, k):
+        if e - b + len(self.path) < k:
+            return
+
         for i in range(b, e):
             self.path.append(i)
             if len(self.path) == k:
-                self.result.append([e for e in self.path])
+                self.result.append([e for e in self.path])  # deepcopy
             else:
                 self.backtracking(i + 1, e, k)
             self.path.pop()
